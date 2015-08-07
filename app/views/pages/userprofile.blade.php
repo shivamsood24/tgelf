ce693fb903f1de6040d7cc40d4f88ac30f689ea2
hemantantialias <hemant@marulaconcepts.com>
2015-08-07 00:06:01 -0700

    profile page

--- app/views/pages/userprofile.blade.php
+++ app/views/pages/userprofile.blade.php
@@ -4,14 +4,38 @@
 <div class="container login-container">
 <div class="row">
 <div class="col-md-4">
-<div  id="cicle"> </div>
+<div  id="circle"> </div>
 	
 </div>
 <div class="col-md-4">
+<h1>JOHN HARRISON</h1>
+<hr>
+Ford Foundation
 	
 </div>
 <div class="col-md-4">
+	<h1>JOHN HARRISON</h1>
+<hr>
+Ford Foundation
+</div>
 	
+</div>
+<div class="row">
+<div class="col-md-4">
+<h1>JOHN HARRISON</h1>
+<hr>
+Ford Foundation	
+</div>
+<div class="col-md-4">
+<h1>BIO</h1>
+<hr>
+<p style="text-align:center">Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data. Often</p>
+	
+</div>
+<div class="col-md-4">
+	<h1>JOHN HARRISON</h1>
+<hr>
+Ford Foundation
 </div>
 	
 </div>
