db320f6c0241419138ed94ec9e107fde380a55b0
Pawan Kumar <surpawan@gmail.com>
2015-08-07 10:51:00 +0530

    fixed routes

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -56,7 +56,7 @@ return array(
 	"not_in"               => "The selected :attribute is invalid.",
 	"numeric"              => "The :attribute must be a number.",
 	"regex"                => "The :attribute format is invalid.",
-	"required"             => "The :attribute field is required.",
+	"required"             => "Field is required.",
 	"required_if"          => "The :attribute field is required when :other is :value.",
 	"required_with"        => "The :attribute field is required when :values is present.",
 	"required_with_all"    => "The :attribute field is required when :values is present.",
