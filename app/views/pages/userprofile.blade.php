5f5fa29c68614bc78cde124e33f4fde02e82b439
hemantantialias <hemant@marulaconcepts.com>
2015-08-07 14:47:41 +0530

    fixing register

--- app/views/pages/userprofile.blade.php
+++ app/views/pages/userprofile.blade.php
@@ -19,10 +19,11 @@
 				</div>
 				
 			</div>
+			<br>
 			<div class="row">
 			<div class="col-md-4">
 				<h3>AREAS OF INTEREST</h3>
-				<hr>
+				<a href="#" ><img src="{{asset('uploads/line.png')}}" style="width:100%;"></a>
 				 <a href="#" class="soclicon">Design</a>
                 <a href="#" class="soclicon">Technology</a>
                 <a href="#" class="soclicon">Research</a>
@@ -35,13 +36,14 @@
 				</div>
 				<p>/</p>
 				<div class="col-md-3">
-					<p class="profilep">"Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data. Often"</p>
+					<p class="profilep">"Lorem ipsum” dummy text is used by many web-developers to test."</p>
+					<div style="margin-top: 10%;">
 				<a href="../icon/github" class="soclicon"><i class="icon-github"></i></a>
 				<a href="../icon/github" class="soclicon"><i class="icon-github"></i></a>
 				<a href="../icon/github" class="soclicon"><i class="icon-youtube-play"></i></a>
 				<a href="../icon/github" class="soclicon"><i class="icon-twitter-sign"></i></a>
 				<a href="../icon/github" class="soclicon"><i class="icon-linkedin-sign"></i></a>
-				
+				</div>
                
                             
             
