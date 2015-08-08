d5769788ced8b4d98903ff79f7b278cf3bdb6941
hemantantialias <hemant@marulaconcepts.com>
2015-08-08 14:06:54 +0530

    changes

--- app/views/pages/showleaders.blade.php
+++ app/views/pages/showleaders.blade.php
@@ -2,56 +2,119 @@
 @section('content')
 
         <div class="container login-container">
+        <p style="text-align: center;    margin-bottom: -21px;">Leaders Forum</p>
+
+        <h1 id="war-header" style="text-align: center;"><span id="war-header-inner">UNITED STATES</span>
+        </h1>
+
             <div class="row">
                 <div class="col-md-4">
-                <div  id="circles" class="left"><h4>User Name</h4></div>
-                <div id="right">
-                    
-                </div>
-                </div>
-                <div class="col-md-5">
-                <h1></h1>
-                <hr>
-                <h3>Ford Foundation</h3>
-                <h3>Regional Vice President</h3>
-                    
-                </div>
                 <div class="col-md-3">
-                    <div  id="circle"> </div>
+                    <div id="circles" class="left"></div>
                 </div>
-                
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                     <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
             </div>
             <br>
             <div class="row">
-            <div class="col-md-4">
-                <h3>AREAS OF INTEREST</h3>
-                <a href="#" ><img src="{{asset('uploads/line.png')}}" style="width:100%;"></a>
-                 <a href="#" class="soclicon">Design</a>
-                <a href="#" class="soclicon">Technology</a>
-                <a href="#" class="soclicon">Research</a>
-                </div>
-                <div class="col-md-5" style="border-right: 1px #A83334 solid;border-left: 1px #A83334 solid;">
-                
-                <h3 style="text-align: center">BIO</h3>
-                
-                <p style="text-align:center" class="profilep">Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data. Often</p>
-                
-                </div>
-                
+                <div class="col-md-4">
                 <div class="col-md-3">
-                    <p class="profilep">"Lorem ipsum” dummy text is used by many web-developers to test."</p>
-                    <div style="margin-top: 10%;">
-                <a href="../icon/github" class="soclicon"><i class="icon-github"></i></a>
-                <a href="../icon/github" class="soclicon"><i class="icon-github"></i></a>
-                <a href="../icon/github" class="soclicon"><i class="icon-youtube-play"></i></a>
-                <a href="../icon/github" class="soclicon"><i class="icon-twitter-sign"></i></a>
-                <a href="../icon/github" class="soclicon"><i class="icon-linkedin-sign"></i></a>
+                    <div id="circles" class="left"></div>
                 </div>
-               
-                            
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
             
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
                 </div>
-                
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="col-md-3">
+                    <div id="circles" class="left"></div>
+                </div>
+                <div id="right" class="col-md-9">
+                    <h4>User Name</h4>
+                    <hr>
+                    <p>Ford Foundation</p>
+                    <p>Regional Vice President</p>
+                </div>
+                </div>
+            <br>
             </div>
         </div>
 @stop
