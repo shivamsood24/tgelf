ab2c7cce5c76a7bd6a86dd25f97d22de29cae5a5
hemantantialias <hemant@marulaconcepts.com>
2015-08-10 20:57:45 +0530

    program page

--- app/views/pages/program.blade.php
+++ app/views/pages/program.blade.php
@@ -6,25 +6,24 @@
     <br>
     <br>
     <h1 align="center" style="color: #A83334;">" Our vision is strategic,our voice credible,our results are unmistakable and our impact global."</h1>
-    <hr style="border-top: 3px solid #194759;">
     <br><br><br><br>
     <div class="row">
         <div class="col-md-3" style="text-align: center;">
-        <img src="{{asset('uploads/pro1.png')}}">
-        <input class="btn btn-default login-btn last-btn next program" type="submit" value="COMMUNITY">
+            <img src="{{asset('uploads/pro1.png')}}">
+            <input class="btn btn-default login-btn last-btn next program" type="submit" value="COMMUNITY">
         </div>
         <div class="col-md-3" style="text-align: center;">
-        <img src="{{asset('uploads/pro2.png')}}">
+            <img src="{{asset('uploads/pro2.png')}}">
             <a href="#reg1"> <input class="btn btn-default login-btn next1 program" type="submit" value="OPPORTUNITIES "></a>
 
         </div>
         <div class="col-md-3" style="text-align: center;">
-        <img src="{{asset('uploads/pro3.png')}}">
+            <img src="{{asset('uploads/pro3.png')}}">
             <input class="btn btn-default login-btn next2 last-btn next2 program" type="submit" value="PARTNERSHIP">
 
         </div>
         <div class="col-md-3" style="text-align: center;">
-        <img src="{{asset('uploads/pro4.png')}}">
+            <img src="{{asset('uploads/pro4.png')}}">
             <a href="#reg1"><input class="btn btn-default login-btn next3 program" type="submit" value="PERSONAL DEVELOPMENT"></a>
         </div>
 
@@ -32,47 +31,52 @@
     </div>
 
     <div class="container login-container">
-     <ul id="progressbar1">
+       <ul id="progressbar1">
         <li class="l1 active">PRE COLLEGE</li>
         <li class="l2">FRESHMAN</li>
         <li class="l3">JUNIOR</li>
         <li class="l4">SENIOR</li>
     </ul>
+    <br><br>
     <fieldset class="f1" >
         <div class="row col-md-12 ">
-             <h1 align="center" style="color: #A83334;">"A week long outward bound trip, focussing on service, and also
-on bonding within the new batch of the leaders forum members."</h1>
-    <hr style="border-top: 3px solid #194759;">
+            <p class="programp">"A week long outward bound trip, focussing on service, and also
+                on bonding within the new batch of the leaders forum members."</p>
+                <br>
+                
 
-        </div>
-    </fieldset>
-        <fieldset class="f2" style="display:none">
-            <div class="row" id="#reg1">
-               <h1 align="center" style="color: #A83334;">"Every forum member chooses one of these summers 
-and works with TGELF for 6 weeks. Two weeks of this 6 week program will emphasise 
-learning. With one week will be devoted to internal training, while the second will focus 
-on external leadership. The remaining 4 weeks will involve working with the foundation."</h1>
-    <hr style="border-top: 3px solid #194759;">
             </div>
         </fieldset>
-    <fieldset class="f3" style="display:none">
-        <div class="row">
-            
-             <h1 align="center" style="color: #A83334;">"tGELF helps the members of the leaders forum in getting internship 
-opportunities in their areas of interest, through a structured and a regulated process."</h1>
-    <hr style="border-top: 3px solid #194759;">
-        </div>
-    </fieldset>
-    <fieldset class="f4" style="display:none">
-        <div class="row">
-            
-             <h1 align="center" style="color: #A83334;">"After graduation, tGELF offers a fellowship programme to interested
-students. This fellowship programme would be a 2 year long programme, which would 
-involve ...."</h1>
-    <hr style="border-top: 3px solid #194759;">
-        </div>
-    </fieldset>
-          </div>
+        <fieldset class="f2" style="display:none">
+            <div class="row" id="#reg1">
+              <p class="programp">"Every forum member chooses one of these summers 
+                and works with TGELF for 6 weeks. Two weeks of this 6 week program will emphasise 
+                learning. With one week will be devoted to internal training, while the second will focus 
+                on external leadership. The remaining 4 weeks will involve working with the foundation."</p>
+                <br>
+                
+            </div>
+        </fieldset>
+        <fieldset class="f3" style="display:none">
+            <div class="row">
+                
+               <p class="programp">"tGELF helps the members of the leaders forum in getting internship 
+                opportunities in their areas of interest, through a structured and a regulated process."</p>
+                <br>
+                
+            </div>
+        </fieldset>
+        <fieldset class="f4" style="display:none">
+            <div class="row">
+                
+               <p class="programp">"After graduation, tGELF offers a fellowship programme to interested
+                students. This fellowship programme would be a 2 year long programme, which would 
+                involve ...."</p>
+                <br>
+                
+            </div>
+        </fieldset>
     </div>
-    {{ HTML::script('assets/js/program.js') }}
-    @stop
+</div>
+{{ HTML::script('assets/js/program.js') }}
+@stop
--- public/assets/js/program.js
+++ public/assets/js/program.js
@@ -1,41 +1,60 @@
 $('.next').click(function(){
 	$('.l1').addClass('active');
-	$('.f1').show("fade");
-	$('.f2').hide("fade");
-	$('.f3').hide("fade");
-	$('.f4').hide("fade");
+	$('.l2').removeClass('active');
+	$('.l3').removeClass('active');
+	$('.l4').removeClass('active');
+	$('.f1').addClass('showcls');
 	$('.f2').addClass('addcls');
 	$('.f3').addClass('addcls');
 	$('.f4').addClass('addcls');
+	$('.f2').removeClass('showcls');
+	$('.f3').removeClass('showcls');
+	$('.f4').removeClass('showcls');
+
 	
 });
 
 $('.next1').click(function(){
-			$('.l1').removeClass('active');
-			$('.l3').removeClass('active');
-			$('.l4').removeClass('active');
-			$('.l2').addClass('active');
-			$('.f1').addClass('addcls');
-			$('.f2').show("fade");
-			$('.f3').addClass('addcls');
-			$('.f4').addClass('addcls');
+	$('.l2').addClass('active');
+	$('.l1').removeClass('active');
+	$('.l3').removeClass('active');
+	$('.l4').removeClass('active');
+	$('.f1').addClass('addcls');
+	$('.f2').addClass('showcls');
+	$('.f3').addClass('addcls');
+	$('.f4').addClass('addcls');
+	$('.f1').removeClass('showcls');
+	$('.f3').removeClass('showcls');
+	$('.f4').removeClass('showcls');
 });
 
 
 $('.next2').click(function(){
-	$('.l2').removeClass('active');
 	$('.l3').addClass('active');
-	$('.f1').hide("fade");
-	$('.f2').hide("fade");
-	$('.f3').show("fade");
+	$('.l1').removeClass('active');
+	$('.l2').removeClass('active');
+	$('.l4').removeClass('active');
+	$('.f1').addClass('addcls');
+	$('.f2').addClass('addcls');
+	$('.f3').addClass('showcls');
+	$('.f4').addClass('addcls');
+	$('.f1').removeClass('showcls');
+	$('.f2').removeClass('showcls');
+	$('.f4').removeClass('showcls');
 
 });
 
 $('.next3').click(function(){
+	$('.l4').addClass('active');
+	$('.l1').removeClass('active');
 	$('.l2').removeClass('active');
-	$('.l3').addClass('active');
-	$('.f1').hide("fade");
-	$('.f2').hide("fade");
-	$('.f3').show("fade");
+	$('.l3').removeClass('active');
+	$('.f1').addClass('addcls');
+	$('.f2').addClass('addcls');
+	$('.f3').addClass('addcls');
+	$('.f4').addClass('showcls');
+	$('.f1').removeClass('showcls');
+	$('.f2').removeClass('showcls');
+	$('.f3').removeClass('showcls');
 
 });
